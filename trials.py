0f0a9ae5f4ddd924774e893872f276723a4768a7
Ioannis Apostolopoulos <apostolopoulos.john.dim@gmail.com>
2020-04-15 21:44:31 +0300

    Add files via upload

--- trials.py
+++ trials.py
@@ -4,24 +4,36 @@ from copy import deepcopy
 
 
 
+
+
+
+
+
+
+
 function  = 'sigmoid' #or 'tanh'
 
-inputs = [[0.8,0.300,0.200,0.750],
-          [0.8,0.500,0.500,0.750],
-          [0.8,0.200,0.100,0.750]] #experimental input matrix for 2 iteration steps (1st row is the initial state)
+inputs = [[0.03,   0.046,   5,   0.75,   0.50,    0.55,    0.70],
+          [0.05,   0.056,   6,  0.75,   0.50,   0.55,    0]] #experimental input matrix for 2 iteration steps (1st row is the initial state)
 
-w = [[0,  0.5, 0, 0],
-     [0.2,  0, 0, 0.900],
-     [0.2,  0, 0, 0.900],
-     [0,  0, 0, 0]] # wxperimental weight matrix
+w = [[0,  0, 0, 0, 0, 0, 0.95],
+     [0,  0, 0, 0, 0, 0, 0.75],
+     [0,  0, 0, 0, 0, 0, 0.5],
+     [0,  0, 0, 0, 0, 0, -0.5],
+     [0,  0, 0, 0, 0, 0, -0.75],
+     [0,  0, 0, 0, 0, 0, -0.75],
+     [0,  0, 0, 0, 0, 0, 0]] # wxperimental weight matrix
 
-inputs = np.array(inputs)
+#y=np.array([np.array(xi) for xi in inputs])
+inputs = np.array(inputs, dtype='float64')
 inputa = deepcopy(inputs)
 w = np.array(w)
 
-#%%
 slope = 1.5 #experimental sigmoid slope
 
+    
+    
+    
 #%% Concept value Calculator: give the row of instances before and after, give the weight matrix, give the excact concept t that you refer to
 def calc_value (t, w, instance, instancebef): # t is an integer describing which concept we are calculating, w is the weight matrix, instance is the row with the initial values of the concepts
     
@@ -50,7 +62,7 @@ def calc_value (t, w, instance, instancebef): # t is an integer describing which
     else:
         ak1n = 1/(1 + math.exp(-slope*ak1)) #if disturbance, return this
         
-    ak1n = ak1
+    #ak1n = ak1
     #ak3 = math.tanh(ak1)
     #ak2 = 1 / (1 + math.exp(-sumw*ak1))
     return ak1n
@@ -58,13 +70,13 @@ def calc_value (t, w, instance, instancebef): # t is an integer describing which
 
 
 def iter_step (inputw, w, nonew): # nonew: if == 0, then it will calculate the first iteration step (i.e. from row 1 to row 2). If == 1, it will calculate from row 1 to row 2
-
+    # inputw = array of inputs per time (we need two rows to do this). the row is defined by the nonew
     instancebef = deepcopy(inputw[nonew,:])
     instance = deepcopy(inputw [nonew+1, :])
     
     new_bef = deepcopy(instance)
     
-    for i in range (np.size(inputw,1)): #for every concept in inputs
+    for i in range (np.size(inputw,1)): #for every concept in inputs, calculate every new value
 
         new_i = calc_value(i+1, w, instance, instancebef) #calls the calc_value function. the first argument is the concept number for calculation
         instance[i] = new_i # change the instance
@@ -73,9 +85,9 @@ def iter_step (inputw, w, nonew): # nonew: if == 0, then it will calculate the f
 
 #%%
 
-in1 = inputs[:,:]                         
+in1 = inputs[:]                         
 
-for k in range (0,50):
+for k in range (0,2):
     
     if k == 0:
         new,bef = iter_step (in1,w,0)
@@ -93,7 +105,6 @@ new = new[np.newaxis]
 bef = bef[np.newaxis]
 in_new = np.concatenate([bef,new],0)
 #%%
-
 
 
 
